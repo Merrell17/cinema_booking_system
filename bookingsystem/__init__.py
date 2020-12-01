b171d2ac2b8ba375db7f033f78a645a8fd55815d
Merrell17 <hughmerrell@me.com>
2020-12-01 09:39:30 +0000

    nav bar progress, add screen working

--- bookingsystem/__init__.py
+++ bookingsystem/__init__.py
@@ -15,7 +15,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 mysql = MySQL()
 mysql.init_app(app)
 
-address_cinema_id = 6
+address_cinema_id = 7
 
 
 @app.route('/addcinema', methods=['GET', 'POST'])
@@ -39,7 +39,7 @@ def add_cinema():
 
         mysql.connection.commit()
         cur.close()
-        address_cinema_id += 6
+        address_cinema_id += 1
         return "success"
     return render_template('addCinema.html')
 
@@ -66,6 +66,12 @@ def add_screen():
     else:
         cinema_details = []
 
+    films = cur.execute("SELECT * FROM movie")
+    if films > 0:
+        film_details = cur.fetchall()
+    else:
+        film_details = []
+
     if request.method == "POST":
         screen_details = request.form
         screen_name = screen_details['name']
@@ -73,14 +79,48 @@ def add_screen():
         column_count = screen_details['column_count']
         cinema_name = screen_details['cinemas']
 
-
-        cinema_id = cur.execute("SELECT id FROM Cinema WHERE name IS %s", [cinema_name])
-
-
         cur.execute("INSERT INTO auditorium(screen_name, cinema_id, row_count, column_count) "
-                    "VALUES(%s, %s, %s, %s)", (screen_name, cinema_id, row_count, column_count))
+                    "VALUES(%s, %s, %s, %s)", (screen_name, cinema_name, row_count, column_count))
 
-    return render_template('addScreens.html', cinemaDetails=cinema_details)
+        mysql.connection.commit()
+        cur.close()
+
+    return render_template('addScreens.html', cinemaDetails=cinema_details,)
+
+
+@app.route('/addfilm', methods=['GET', 'POST'])
+def add_film():
+    if request.method == "POST":
+        cur = mysql.connection.cursor()
+        film_details = request.form
+        title = film_details['title']
+        director = film_details['director']
+        description = film_details['description']
+        duration = film_details['duration']
+
+        cur.execute("INSERT INTO movie(title, director, description, duration_min) "
+                    "VALUES(%s, %s, %s, %s)", (title, director, description, duration))
+
+        mysql.connection.commit()
+        cur.close()
+    return render_template('addFilm.html')
+
+@app.route('/createscreening', methods=['GET', 'POST'])
+def create_screening():
+    cur = mysql.connection.cursor()
+    cinemas = cur.execute("SELECT * FROM cinema")
+    if cinemas > 0:
+        cinema_details = cur.fetchall()
+    else:
+        cinema_details = []
+
+    films = cur.execute("SELECT * FROM movie")
+    if films > 0:
+        film_details = cur.fetchall()
+    else:
+        film_details = []
+
+    return render_template('createScreening.html', cinemaDetails=cinema_details, filmDetails=film_details)
 
 
 if __name__ == '__main__':
