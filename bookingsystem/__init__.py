c7cdd902271100c022cd32d4347de12c657fc0b1
Merrell17 <hughmerrell@me.com>
2021-03-01 11:15:46 +0000

    fixed side bar being overlapped by form entries using css z-index property. Some comments added noting app configuration for pythonanywhere #28

--- bookingsystem/__init__.py
+++ bookingsystem/__init__.py
@@ -11,21 +11,24 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     admin = Admin(app, name='microblog', template_mode='bootstrap3')
     app.config['SECRET_KEY'] = 'key'
-    app.config['MYSQL_USER'] = 'Merrell17'
+
+    # Local user: 'root'
+    # Pythonanywhere user: 'Merrell17'
+    app.config['MYSQL_USER'] = 'root'
 
     # Pythonanywhere pwd: 'root1234'
     # AWS pwd: '12345678'
     # Local pwd: '1234'
-    app.config['MYSQL_PASSWORD'] = 'root1234'
+    app.config['MYSQL_PASSWORD'] = '1234'
 
     # Pythonanywhere dbname = 'Merrell17$flaskapp'
     # Local dbname: = 'flaskapp'
-    app.config['MYSQL_DB'] = 'Merrell17$flaskapp'
+    app.config['MYSQL_DB'] = 'flaskapp'
 
     # AWS host: 'awsflaskbooking.cicsvmdk9o8l.eu-west-2.rds.amazonaws.com'
     # Pythonanywhere host: 'Merrell17.mysql.pythonanywhere-services.com'
     # Local host : 'localhost'
-    app.config['MYSQL_HOST'] = 'Merrell17.mysql.pythonanywhere-services.com'
+    app.config['MYSQL_HOST'] = 'localhost'
     app.config['IMAGE_UPLOADS'] = os.path.abspath("C:/Users/Hugh/PycharmProjects/New folder/cinema_booking_system/bookingsystem/static/imgs")
 
     if test_config is None:
